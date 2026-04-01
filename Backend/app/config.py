3ebedfe13554c383f535424fe62ebe261b5c596d
jpfool <jpfool93@gmail.com>
2026-04-01 11:48:55 +0530

    Update frontend url

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:admin@localhost:5432/pyrochats"
+    DATABASE_URL: str = "postgresql://postgres:sdvIpScbYNiQjQzMuvowmOiynxxsTKnZ@hopper.proxy.rlwy.net:30761/railway"
     SECRET_KEY: str = "your-secret-key"
     TWILIO_ACCOUNT_SID: str = ""
     TWILIO_AUTH_TOKEN: str = ""
     TWILIO_WHATSAPP_NUMBER: str = ""
-    FRONTEND_URL: str = "http://localhost:3000"
+    FRONTEND_URL: str = "https://frontend-production-814b2.up.railway.app"
 
     class Config:
         env_file = ".env"
